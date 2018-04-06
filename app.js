97a686b8cbc349abc8057e0f74389c3dbeef6fab
Romy Greenfield <romygreenfield@Romys-MacBook-Pro.local>
2018-04-06 16:42:06 +0100

    AUDIO ADDED

--- app.js
+++ app.js
@@ -9,18 +9,4 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
-app.use('/*', (req, res, next) => {
-    next({ status: 404 });
-});
-
-app.use((err, req, res, next) => {
-    if (err.status === 404) res.render(`character.ejs`, { name: name, video: video });
-    else next(err);
-});
-
-app.use((err, req, res, next) => {
-    if (err.status === 500) res.render(`character.ejs`, { name: name, video: video });
-    else next(err);
-});
-
 module.exports = app;
