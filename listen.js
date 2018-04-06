0d8b93d162d8f2a64bfdf5b82c34495896e4ff76
Romy Greenfield <romygreenfield@Romys-MacBook-Pro.local>
2018-04-06 15:42:04 +0100

    jquery

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const PORT = process.env.PORT || 9090
 app.listen(PORT, () => {
-    console.log('listening on port 9090');
+    console.log(`listening on port ${PORT}`);
 });
